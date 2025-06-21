fee2e5874368df0ee3101c504f5b492b6250b75e
pushpaksrinivas <savitipushpaksrinivasmurthy666@gmail.com>
2025-06-21 21:15:46 +0530

    Encoded-normal

--- firebase-config.js
+++ firebase-config.js
@@ -3,17 +3,19 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
+// Firebase config with environment variables
 export const firebaseConfig = {
-  apiKey: "AIzaSyDsP80XFSykRlteD8LTQaw76TD8AJItMFw",
-  authDomain: "daykart-77771.firebaseapp.com",
-  databaseURL: "https://daykart-77771-default-rtdb.firebaseio.com",
-  projectId: "daykart-77771",
-  storageBucket: "daykart-77771.firebasestorage.app",
-  messagingSenderId: "533666061468",
-  appId: "1:533666061468:web:a40c38b11a93a18e776159",
-  measurementId: "G-X1FCYK4XR1"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
